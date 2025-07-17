1f4812db4d0680ec0c7c182db046e731577f7b13
Keeththi2003 <keeththi2003@gmail.com>
2025-07-18 00:46:27 +0530

    fix: secant stop condtion fixed

--- secant.c
+++ secant.c
@@ -2,18 +2,24 @@
 #include <math.h>
 #define TOL 0.0001
 
-double f(double x) {
-    return x * x * x - 2*x*x - 3;  
+double f(double x)
+{
+    return x * x * x - 2 * x * x - 3;
 }
+
+double absoluteRelativeError(double x0, double x1)
+{
+    return fabs((x1 - x0) / x1) * 100;
+}
+
 double secant(double x0, double x1)
 {
-    double x2;
+    double x2, error;
     int i = 0;
 
-    printf("Iteration\tx0\tf(x0)\tf(x1)\tx1\tx2\n");
+    printf("Iteration\tx0\tf(x0)\tf(x1)\tx1\tx2\tError\n");
 
-    while (fabs(f(x0)) >= TOL)
-
+    do
     {
 
         if (f(x1) - f(x0) == 0.0)
@@ -23,17 +29,15 @@ double secant(double x0, double x1)
         }
 
         x2 = x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0));
-        printf("%d\t\t%.4lf\t%.4lf\t%.4lf\t%.4lf\t%.4lf\n", i + 1, x0, f(x0), f(x1), x1, x2);
 
-        if (fabs(x2 - x1) < TOL)
-        {
-            return x2;
-        }
+        error = absoluteRelativeError(x1, x2);
+        printf("%d\t\t%.4lf\t%.4lf\t%.4lf\t%.4lf\t%.4lf\t%.4lf\n", i + 1, x0, f(x0), f(x1), x1, x2, error);
 
         x0 = x1;
         x1 = x2;
         i++;
-    }
+
+        } while (error > TOL);
 
     return x2;
 }
