165ef9468b26ce7791528d952a6a2aa448507f4f
Keeththi2003 <keeththi2003@gmail.com>
2025-07-18 00:40:07 +0530

    fix: newton raphson stop condition fixed

--- newtonRaphson.c
+++ newtonRaphson.c
@@ -12,34 +12,36 @@ double fDiff(double x)
     return 3 * x * x - 4 * x;
 }
 
+double absoluteRelativeError(double x0, double x1)
+{
+    return fabs((x1 - x0) / x1) * 100;
+}
+
 double newtonRaphson(double x0)
 {
-    double x1;
+    double x1, error;
     int i = 0;
 
-    printf("Iteration\tx0\tf(x0)\tf'(x0)\tx1\n");
+    printf("Iteration\tx0\tf(x0)\tf'(x0)\tx1\tError\n");
 
-    while (fabs(f(x0)) >= TOL)
+    do
     {
 
         if (fDiff(x0) == 0)
         {
             printf("No solution found.\n");
-            return x0;
+            return NAN;
         }
 
         x1 = x0 - f(x0) / fDiff(x0);
 
-        printf("%d\t%.4lf\t%.4lf\t%.4lf\t%.4lf\n", i + 1, x0, f(x0), fDiff(x0), x1);
+        error = absoluteRelativeError(x0, x1);
 
-        if (fabs(x1 - x0) < TOL)
-        {
-            return x1;
-        }
+        printf("%d\t%.4lf\t%.4lf\t%.4lf\t%.4lf\t%.4lf\n", i + 1, x0, f(x0), fDiff(x0), x1, error);
 
         x0 = x1;
         i++;
-    }
+    } while (error > TOL);
 
     return x1;
 }
